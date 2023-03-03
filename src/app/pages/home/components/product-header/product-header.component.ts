eb833cef7e9c58ba7fe28ae69ffcc6d0f42e9dec
Omolara Adebowale <debbiegterra@gmail.com>
2023-03-03 20:11:05 +0100

    feat: :sparkles: Create product header functionality

--- src/app/pages/home/components/product-header/product-header.component.ts
+++ src/app/pages/home/components/product-header/product-header.component.ts
@@ -1,9 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+
+type Sort = 'desc' | 'asc';
 
 @Component({
   selector: 'app-product-header',
   templateUrl: './product-header.component.html'
 })
 export class ProductHeaderComponent {
+  @Output() columnsCountChange = new EventEmitter<number>();
+  sort: Sort = 'desc';
+  itemShowCount = 12;
 
+  onSortUpdated(newSort: Sort): void {
+    this.sort = newSort;
+  }
+
+  onItemsUpdated(newItem: number): void {
+    this.itemShowCount = newItem;
+  }
+
+  onColumnsUpdated(colsNum: number): void {
+    this.columnsCountChange.emit(colsNum);
+  }
 }
