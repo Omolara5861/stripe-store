684d5efbded8e91f4c36ba4bf3df309820fc63bc
Omolara5861 <debbiegterra@gmail.com>
2023-03-06 00:12:35 +0100

    feat: :tada: Start working on cart page

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,9 +1,31 @@
 import { Component } from '@angular/core';
+import { Cart } from '../../shared/models/cart';
+import { CartItem } from '../../shared/models/cart';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html'
 })
 export class CartComponent {
+//Items in cart
+  cart: Cart = {items: [{
+    product: 'string',
+    name: 'string',
+    price: 120,
+    quantity: 1,
+    id: 1
+  }]};
 
+  //Property to use in the table
+  dataSource: Array<CartItem> = [];
+
+  /** Items to display on the table */
+  displayedColumns: Array<string> = [
+    'product',
+    'name',
+    'price',
+    'quantity',
+    'total',
+    'action',
+  ];
 }
