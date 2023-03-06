1b25dd057254482415f730d1d16f62c2d9c8b0d8
Omolara5861 <debbiegterra@gmail.com>
2023-03-06 12:19:14 +0100

    feat: :necktie: Add method to remove item from cart to the cart service

--- src/app/shared/services/cart.service.ts
+++ src/app/shared/services/cart.service.ts
@@ -30,6 +30,21 @@ export class CartService {
     this._snackBar.open('1 item added to cart.', 'Ok', { duration: 3000 });
   }
 
+  removeFromCart(item: CartItem, updateCart = true): CartItem[] {
+    const filteredItems = this.cart.value.items.filter(
+      (_item) => _item.id !== item.id
+    );
+
+    if (updateCart) {
+      this.cart.next({ items: filteredItems });
+      this._snackBar.open('1 item removed from cart.', 'Ok', {
+        duration: 3000,
+      });
+    }
+
+    return filteredItems;
+  }
+
   /** Calculate the total price for the items in the cart */
   getTotal(items: CartItem[]): number {
     return items
