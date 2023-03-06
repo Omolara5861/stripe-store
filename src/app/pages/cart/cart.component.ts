f40769c319169e8b5671c58f2c3303ab20a7638a
Omolara5861 <debbiegterra@gmail.com>
2023-03-06 12:14:25 +0100

    feat: :sparkles: Dynamically add product in cart to cart page

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Cart } from '../../shared/models/cart';
 import { CartItem } from '../../shared/models/cart';
 import { CartService } from '../../shared/services/cart.service';
@@ -7,35 +7,12 @@ import { CartService } from '../../shared/services/cart.service';
   selector: 'app-cart',
   templateUrl: './cart.component.html'
 })
-export class CartComponent {
+export class CartComponent implements OnInit{
   //Items in cart
-  cart: Cart={
-    items: [{
-      product: 'https://via.placeholder.com/150',
-      name: 'string',
-      price: 120,
-      quantity: 1,
-      id: 1
-    }]
-  };
+  cart: Cart={items: [] };
 
   //Property to use in the table
-  dataSource: Array<CartItem>=[
-    {
-      product: 'https://via.placeholder.com/150',
-      name: 'string',
-      price: 120,
-      quantity: 1,
-      id: 1
-    },
-    {
-      product: 'https://via.placeholder.com/150',
-      name: 'string',
-      price: 120,
-      quantity: 3,
-      id: 2
-    }
-  ];
+  dataSource: Array<CartItem>=[];
 
   /** Items to display on the table */
   displayedColumns: Array<string>=[
@@ -47,8 +24,14 @@ export class CartComponent {
     'action',
   ];
 
-  constructor(private cartService: CartService) {
-    
+  constructor(private cartService: CartService) {}
+
+  ngOnInit(): void {
+    this.dataSource = this.cart.items;
+    this.cartService.cart.subscribe(_cart => {
+      this.cart = _cart;
+      this.dataSource = this.cart.items;
+    });
   }
 
   /** Calculate the total price for the items in the cart */
