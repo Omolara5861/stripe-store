7526e4ecab364981abf8bb4647689b2c89449d45
Omolara5861 <debbiegterra@gmail.com>
2023-03-06 11:03:18 +0100

    feat: :necktie: Create add to cart functionality

--- src/app/pages/home/components/product-box/product-box.component.ts
+++ src/app/pages/home/components/product-box/product-box.component.ts
@@ -1,10 +1,28 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, EventEmitter, Output } from '@angular/core';
+import { Product } from '../../../../shared/models/product';
 
 @Component({
   selector: 'app-product-box',
   templateUrl: './product-box.component.html'
 })
 export class ProductBoxComponent {
-
+  //Pass fullWidth property to the parent component home
   @Input() fullWidthMode = false;
+
+  product: Product | undefined = {
+    id: 1,
+    title: 'Sneakers',
+    price: 200,
+    category: 'shoes',
+    description: 'string',
+    image: 'https://via.placeholder.com/200'
+  }
+
+  //Pass event to parent component
+  @Output() addToCart = new EventEmitter<Product>();
+
+  /** Emits the product gotten from the cart */
+  onAddToCart(): void {
+    this.addToCart.emit(this.product);
+  }
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
+import { CartService } from '../../shared/services/cart.service';
+import { Product } from '../../shared/models/product';
 
+/** The height of product box */
 const ROW_HEIGHT: {[id:number]:number} = {1: 400, 3: 335, 4: 350};
 
 @Component({
@@ -11,6 +14,8 @@ export class HomeComponent {
   rowHeight = ROW_HEIGHT[this.cols];
   category: string | undefined;
 
+  constructor(private cartService: CartService) {}
+
     onUpdateColumnsCount(colsCount: number): void {
       this.cols = colsCount;
       this.rowHeight = ROW_HEIGHT[this.cols];
@@ -18,5 +23,16 @@ export class HomeComponent {
 
     onShowCategory(newCategory: string): void {
       this.category = newCategory;
+    }
+
+    onAddToCart(product: Product): void {
+      //Send new properties since the product is different from cart item
+      this.cartService.addToCart({
+        product: product.image,
+        name: product.title,
+        price: product.price,
+        quantity: 1,
+        id: product.id,
+      });
     }
 }
