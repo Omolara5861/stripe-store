1000b4cdaf6e1e8a8d9b30441d634947c9f5ffdd
Omolara5861 <debbiegterra@gmail.com>
2023-03-06 10:54:26 +0100

    feat: :necktie: Create cart service logic

--- src/app/shared/services/cart.service.ts
+++ src/app/shared/services/cart.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { BehaviorSubject } from 'rxjs';
+import { Cart, CartItem } from '../models/cart';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
+  /** @BehaviorSubject holds the initial item / items in cart and updates it with new item added to the cart. 'item === product'. Components can subscribe to it and update the UI subsequently */
+  cart = new BehaviorSubject<Cart>({items: []});
+  constructor(private _snackBar: MatSnackBar) { }
 
-  constructor() { }
+  addToCart(item: CartItem): void {
+    /** New items array destructured from the original items object in order not to alter it*/
+    const items = [...this.cart.value.items];
+
+    /** Checks if item already exist in cart, if it does, the item quantity would be increase else it would add the item to the items array */
+    const itemInCart = items.find((_item) => _item.id === item.id);
+    if (itemInCart) {
+      itemInCart.quantity += 1;
+    } else {
+      items.push(item);
+    }
+
+    /** Emit the items object so that every component that is subscribed to the cart can get the updated items */
+    this.cart.next({ items });
+
+    /**Displays popup with message for 3 secs */
+    this._snackBar.open('1 item added to cart.', 'Ok', { duration: 3000 });
+  }
+
 }
