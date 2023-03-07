22aa81f0824c31b6f03fb680ecceea3955f942ce
Omolara Adebowale <debbiegterra@gmail.com>
2023-03-07 17:00:44 +0100

    feat: :sparkles: Create checkout method for cart page

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { loadStripe } from '@stripe/stripe-js';
 import { Cart, CartItem } from '../../shared/models/cart';
 import { CartService } from '../../shared/services/cart.service';
 
@@ -23,7 +25,7 @@ export class CartComponent implements OnInit{
     'action',
   ];
 
-  constructor(private cartService: CartService) {}
+  constructor(private cartService: CartService, private http: HttpClient) {}
 
   ngOnInit(): void {
     this.dataSource = this.cart.items;
@@ -58,7 +60,17 @@ export class CartComponent implements OnInit{
     this.cartService.clearCart();
   }
 
+  /** This method makes a post request to stripe to process users payment */
   onCheckout(): void {
-    
+    this.http
+      .post('http://localhost:4242/checkout', {
+        items: this.cart.items,
+      })
+      .subscribe(async (res: any) => {
+        let stripe = await loadStripe('your token');
+        stripe?.redirectToCheckout({
+          sessionId: res.id,
+        });
+      });
   }
 }
