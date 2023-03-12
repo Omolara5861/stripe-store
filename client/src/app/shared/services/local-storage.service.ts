9cbb04c0ed5265ea903ab8836b577c3b527e7d01
Omolara5861 <debbiegterra@gmail.com>
2023-03-12 05:31:51 +0100

    feat: :necktie: Create logic that pesist cart items to loalstorage

--- client/src/app/shared/services/local-storage.service.ts
+++ client/src/app/shared/services/local-storage.service.ts
@@ -6,4 +6,29 @@ import { Injectable } from '@angular/core';
 export class LocalStorageService {
 
   constructor() { }
+
+  /**
+   *Saves @param value to local storage under the name @param key
+   * @param key is a string that represents the key or name under which the data will be stored in the local storage
+   * @param value is the data that would be stored in the local storage.
+   */
+  setItem(key: string, value: any): void {
+    localStorage.setItem(key, JSON.stringify(value));
+  }
+
+  /**
+   * @returns Retrieve @param key from local storage
+   */
+  getItem(key: string): any {
+    const value = localStorage.getItem(key);
+    if (value) {
+      return JSON.parse(value);
+    }
+    return null;
+  }
+
+  /**Clear the local storage */
+  clear(): void {
+    localStorage.clear();
+  }
 }
